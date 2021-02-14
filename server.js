508ef38dd69d652f4ad621342f5f9f15abe643ed
Luemigues <lulimgues@hotmail.com>
2021-02-14 14:50:37 +0100

    cors

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const configRoutes = require('./route/configRoutes')
 
 // Routes
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.use('/search', configRoutes);
 
 const port = process.env.PORT || 3003
